1428dbedb403bee0d88c54638d376058321449d1
Simon Low <simonlow@unknown.home>
2026-07-30 11:52:26 +0100

    Add RANKD of the Day to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,60 @@
-import Hero from "@/components/Hero";
-import Trending from "@/components/Trending";
-import Categories from "@/components/Categories";
-import WhyRankd from "@/components/WhyRankd";
-import Footer from "@/components/Footer";
+import Hero from "@/components/Hero"
+import DailyRankd from "@/components/DailyRankd"
+import Trending from "@/components/Trending"
+import Categories from "@/components/Categories"
+import WhyRankd from "@/components/WhyRankd"
+import Footer from "@/components/Footer"
+
 
 export default function Home() {
+
+
   return (
-    <main>
+
+    <main className="
+      min-h-screen
+      bg-black
+      text-white
+    ">
+
+
       <Hero />
-      <Trending />
-      <Categories />
-      <WhyRankd />
+
+
+
+      <div className="
+        max-w-6xl
+        mx-auto
+        px-6
+        py-16
+      ">
+
+
+        <DailyRankd />
+
+
+
+        <Trending />
+
+
+
+        <Categories />
+
+
+
+        <WhyRankd />
+
+
+
+      </div>
+
+
+
       <Footer />
+
+
     </main>
-  );
+
+  )
+
 }
