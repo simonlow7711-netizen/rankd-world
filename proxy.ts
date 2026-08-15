a0156889d1d1d19eff7426bfffedd4c144135db2
Simon Low <simonlow@unknown.home>
2026-08-15 19:41:09 +0100

    Fix RANKD social image generation

--- proxy.ts
+++ proxy.ts
@@ -1,18 +1,34 @@
-import { createServerClient } from "@supabase/ssr"
-import { NextResponse } from "next/server"
+import {
+  createServerClient
+} from "@supabase/ssr"
 
-import type { NextRequest } from "next/server"
+
+import {
+  NextResponse
+} from "next/server"
+
+
+import type {
+  NextRequest
+} from "next/server"
+
+
 
 
 
 export async function proxy(
+
   request: NextRequest
+
 ){
 
 
   let response =
+
     NextResponse.next({
+
       request
+
     })
 
 
@@ -20,6 +36,7 @@ export async function proxy(
 
 
   const supabase =
+
     createServerClient(
 
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -41,7 +58,16 @@ export async function proxy(
 
 
             cookiesToSet.forEach(
-              ({name,value,options})=>{
+
+              ({
+
+                name,
+
+                value,
+
+                options
+
+              }) => {
 
 
                 request.cookies.set({
@@ -65,6 +91,7 @@ export async function proxy(
 
 
               }
+
             )
 
 
@@ -97,9 +124,9 @@ export async function proxy(
 
 export const config = {
 
-  matcher:[
+  matcher: [
 
-    "/((?!_next/static|_next/image|favicon.ico).*)"
+    "/((?!_next/static|_next/image|favicon.ico|api/og).*)"
 
   ]
 
